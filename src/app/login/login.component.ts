713612c2e5752fcfbb9d5a55dbbf86c4cb45062c
Christian Pillaga <christian_pillaga_s@hotmail.es>
2021-03-18 12:49:19 -0500

    pa

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -36,7 +36,9 @@ export class LoginComponent implements OnInit {
     this._autenticacionService.login(forma.value.email, forma.value.password)
       .subscribe(correcto => {
 
+        console.log(correcto);
         this._generalService.getSucursal(correcto.user.branchOffice).subscribe(resp => {
+          console.log(resp);
           if(resp[0].status == "true" && resp[0].business.status == "true" ){
 
             localStorage.setItem("sucursalBT", JSON.stringify(resp[0]));
